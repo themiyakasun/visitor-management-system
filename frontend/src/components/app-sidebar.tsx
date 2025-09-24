36bf5d7e11057f87c63df634c26fcdbc8d08d28e
Themiya Kasun <themiyakasun.tb@gmail.com>
2025-09-24 12:51:39 +0530

    Frontend init

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import { GalleryVerticalEnd } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+import { NavUser } from './nav-user';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '/',
+    },
+    {
+      title: 'Master Data',
+      url: '#',
+      items: [
+        {
+          title: 'Departments',
+          url: '#',
+        },
+        {
+          title: 'Roles & Permissions',
+          url: '#',
+          isActive: true,
+        },
+        {
+          title: 'Break Types',
+          url: '#',
+        },
+        {
+          title: 'System Users',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Operations',
+      url: '#',
+      items: [
+        {
+          title: 'Persons',
+          url: '#',
+        },
+        {
+          title: 'Vehicles',
+          url: '#',
+        },
+        {
+          title: 'Appoinments',
+          url: '#',
+        },
+        {
+          title: 'Gate Logs',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Reports',
+      url: '#',
+      items: [
+        {
+          title: 'Active Time Report',
+          url: '#',
+        },
+        {
+          title: 'Time In/Out Report',
+          url: '#',
+        },
+        {
+          title: 'Break Logs Report',
+          url: '#',
+        },
+        {
+          title: 'Appoinment Report',
+          url: '#',
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size='lg' asChild>
+              <a href='#'>
+                <div className='bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
+                  <GalleryVerticalEnd className='size-4' />
+                </div>
+                <div className='flex flex-col gap-0.5 leading-none'>
+                  <span className='font-medium'>Documentation</span>
+                  <span className=''>v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className='font-medium'>
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
